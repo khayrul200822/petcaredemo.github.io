bebc963d81e831822c65c179c412083d8489814d
khayrul200822 <khayrulislam200822@gmail.com>
2024-02-05 12:40:29 +0600

    header uploaded

---
+++ js/script.js
@@ -0,0 +1,16 @@
+
+
+
+// const hover_area1 = document.querySelector(".option-menu");
+// const hover_btn1 = document.querySelector(".language")
+// const hover_btn2 = document.querySelector(".currency")
+
+// const hover_handling = (hover_btn, hover_area) => {
+//     hover_btn.addEventListener("mouseover", () => {
+//         hover_area.style.animation = "fade-up .5s ease-in-out forwards alternate"
+//     })
+//     hover_btn.addEventListener("mouseleave", () => {
+//         hover_area.style.animation = "fade-down .5s ease-in-out forwards alternate"
+//     })
+// }
+
