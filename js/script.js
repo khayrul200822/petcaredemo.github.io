e312111f2750a7f054ad6e9c3950d8b6ab46619f
khayrul200822 <khayrulislam200822@gmail.com>
2024-02-07 19:08:32 +0600

    update header style

--- js/script.js
+++ js/script.js
@@ -3,10 +3,10 @@ const newsletter_poup_close = document.querySelector(".newsletter_close");
 const newsletter_popup = document.querySelector(".newsletter_popup")
 
 setTimeout(() => {
-  newsletter_popup.style.transform= "scale(1)";
-   },5000)
+  newsletter_popup.style.transform = "scale(1)";
+}, 5000)
 newsletter_poup_close.addEventListener("click", () => {
-  newsletter_popup.style.transform= "scale(0)";
+  newsletter_popup.style.transform = "scale(0)";
 })
 
 
@@ -15,28 +15,56 @@ const increase_button = document.querySelectorAll(".increase");
 const decrease_button = document.querySelectorAll(".decrease");
 const cart_value_input = document.querySelectorAll(".cart_value");
 
-increase_button.forEach((btn , index) => {
-  btn.addEventListener("click", () => { 
+increase_button.forEach((btn, index) => {
+  btn.addEventListener("click", () => {
     let inputValue = parseInt(cart_value_input[index].value);
-    cart_value_input[index].value = inputValue+1;
-   
+    cart_value_input[index].value = inputValue + 1;
+
   })
 })
 decrease_button.forEach((btn, index) => {
   btn.addEventListener("click", () => {
     let input_value = parseInt(cart_value_input[index].value);
-    if(input_value > 1)
-    cart_value_input[index].value = input_value-1;
+    if (input_value > 1)
+      cart_value_input[index].value = input_value - 1;
   })
 })
 
-  // accordian script
-  $(".accordion-content").css("display", "none");
+// accordian script
+$(".accordion-content").css("display", "none");
 
-  $(".accordion-title").click(function () {
-     $(".accordion-title").not(this).removeClass("open");
-     $(".accordion-title").not(this).next().slideUp(300);
-     $(this).toggleClass("open");
-     $(this).next().slideToggle(300);
-  });
+$(".accordion-title").click(function () {
+  $(".accordion-title").not(this).removeClass("open");
+  $(".accordion-title").not(this).next().slideUp(300);
+  $(this).toggleClass("open");
+  $(this).next().slideToggle(300);
+});
 
+
+const swiper = new Swiper('.swiper', {
+  loop: true,
+  slidesPerView: 1,
+  spaceBetween: 20,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  // breakpoints: {
+  //   640: {
+  //     slidesPerView: 2,
+  //     spaceBetween: 20,
+  //   },
+  //   768: {
+  //     slidesPerView: 4,
+  //     spaceBetween: 40,
+  //   },
+  //   1024: {
+  //     slidesPerView: 5,
+  //     spaceBetween: 50,
+  //   }
+  // }
+});
