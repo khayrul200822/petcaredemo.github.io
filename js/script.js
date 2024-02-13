98d6d8bac501d25d8c2eff6b197213dacfcc8921
khayrul200822 <khayrulislam200822@gmail.com>
2024-02-13 18:24:39 +0600

    updated style

--- js/script.js
+++ js/script.js
@@ -75,6 +75,17 @@ appo_popup_close.addEventListener("click", () => {
 })
 
 const swiper = new Swiper('.swiper', {
+  effect: 'creative',
+  creativeEffect: {
+    prev: {
+      // will set `translateZ(-400px)` on previous slides
+      translate: [0, 0, -400],
+    },
+    next: {
+      // will set `translateX(100%)` on next slides
+      translate: ['100%', 0, 0],
+    },
+  },
   loop: true,
   slidesPerView: 1,
   spaceBetween: 20,
